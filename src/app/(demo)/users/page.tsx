d42277f34ef6a97a6756a6313f9398d46f64024c
gauravshilpakar <gaurav.shilpakar@gmail.com>
2024-09-04 23:09:59 -0400

    fixed placeholder content

--- src/app/(demo)/users/page.tsx
+++ src/app/(demo)/users/page.tsx
@@ -1,15 +1,4 @@
-import Link from "next/link";
-
-import PlaceholderContent from "@/components/demo/placeholder-content";
 import { ContentLayout } from "@/components/admin-panel/content-layout";
-import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 
 export default function UsersPage() {
     return (
